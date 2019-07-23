b5713ab3504dcb76aae9e95c62bafc02248b0087
Zsofia <oroszi.zsofi@gmail.com>
2019-07-23 14:32:34 +0200

    Add new items to the UI

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ var budgetController = (function() {
         this.description = description;
         this.value = value;
     };
-
     var data = {
         allItems: {
             exp: [],
-            inc[]
-        }
+            inc: []
+        },
         totals: {
             exp: 0,
             inc: 0
@@ -60,9 +59,11 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
-
-    }
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list',
+    };
+    
     return {
         getInput: function() {
             return {
@@ -71,14 +72,35 @@ var UIController = (function() {
                 value: document.querySelector(DOMStrings.inputValue).value
             };
         },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            // Create an HTML string with placeholder text
+
+            if (type === 'inc') {
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMStrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+        
+            // Replace the placeholder text with actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
         getDOMStrings: function() {
             return DOMStrings;
         }
     };
 
 })();
-
-
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
@@ -102,10 +124,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         var input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
-        ver newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
-
+        UICtrl.addListItem(newItem, input.type);
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
