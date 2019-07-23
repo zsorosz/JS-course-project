c2967247884f0c1974ec93d481fdefc8d3819739
Zsofia <oroszi.zsofi@gmail.com>
2019-07-23 19:27:52 +0200

    Clear input fields

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ var UIController = (function() {
         incomeContainer: '.income__list',
         expensesContainer: '.expenses__list',
     };
-    
+
     return {
         getInput: function() {
             return {
@@ -93,6 +93,20 @@ var UIController = (function() {
 
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            })
+
+            fieldsArr[0].focus();
         },
 
         getDOMStrings: function() {
@@ -128,6 +142,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
+
+        // 4. Clear the fields
+        UICtrl.clearFields();
+
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
